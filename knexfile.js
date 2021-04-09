6f1c327a0400664791e16dab5693cd693454f82c
Guilherme-Vecchi <guivecchi12@gmail.com>
2021-04-09 17:53:59 -0600

    added ssl to knexfile > production

--- knexfile.js
+++ knexfile.js
@@ -15,7 +15,10 @@ module.exports = {
   
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      connectionString:process.env.DATABASE_URL,
+      ssl: {rejectUnauthorized: false}
+    },
     pool: {
       min: 2,
       max: 10
