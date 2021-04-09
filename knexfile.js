846497e881deb16605b052b063ed2cebf7552e94
Guilherme-Vecchi <guivecchi12@gmail.com>
2021-04-09 17:36:33 -0600

    knexfile production conection changes

--- knexfile.js
+++ knexfile.js
@@ -3,41 +3,26 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'sqlite',
+    connection: { filename: './data/tracker.db3' },
     pool: {
-      min: 2,
-      max: 10
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done)
+      }
     },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    useNullAsDefault: true
   },
-
+  
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tablename: 'knex_migrations',
+      directory: './migrations'
     }
   }
 
