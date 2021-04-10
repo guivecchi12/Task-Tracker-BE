dd6e243bf1c699779a928e3928ea2a19c72f57fc
Guilherme-Vecchi <guivecchi12@gmail.com>
2021-04-10 09:14:58 -0600

    added one more project to seed

--- seeds/project.js
+++ seeds/project.js
@@ -1,5 +1,6 @@
 const proj = [
-  {name:'Test Tables', dep_id: 1, user_id: 1}
+  {name:'Test Tables', dep_id: 1, user_id: 1},
+  {name:'Test Tables2', dep_id: 1, user_id: 1}
 ]
 exports.seed = function(knex) {
   // Deletes ALL existing entries
