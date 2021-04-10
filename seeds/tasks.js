549ec272b0abfb822b20a79f0ad83764836131a4
Guilherme-Vecchi <guivecchi12@gmail.com>
2021-04-10 09:38:27 -0600

    added one more project to seed

--- seeds/tasks.js
+++ seeds/tasks.js
@@ -1,6 +1,6 @@
 const tasks = [
-  {name: 'users table', description: 'needs to contain: id, name, email, picture and department they belong to', proj_id: 1},
-  {name: 'department table', description: 'needs to contain: id, name, image', proj_id: 1},
+  {name: 'users table', description: 'needs to contain: id, name, email, picture and department they belong to', proj_id: 2},
+  {name: 'department table', description: 'needs to contain: id, name, image', proj_id: 2},
 ]
 exports.seed = function(knex) {
   // Deletes ALL existing entries
