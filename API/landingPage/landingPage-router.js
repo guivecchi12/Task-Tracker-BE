f0c3d74742f1871dba3ecde787857adf7ec5592d
Guilherme-Vecchi <guivecchi12@gmail.com>
2021-04-09 19:23:39 -0600

    landing page

--- API/landingPage/landingPage-router.js
+++ API/landingPage/landingPage-router.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 router.get('/', async(req, res) => {
     try{
-        return res.status(200).json({message: "Welcome"})
+        return res.status(200).json({message: "Welcome to my tasks"})
     }
     catch(err){
         return res.status(500).json({ message: err.message })
