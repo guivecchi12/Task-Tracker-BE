d57250c9036f21814632a58cf44109bc93b9ec34
Guilherme-Vecchi <guivecchi12@gmail.com>
2021-04-22 13:17:43 -0600

    fixed projects to pass as array of objects and not to have projects in side it passing the objects

--- API/projects/projects-router.js
+++ API/projects/projects-router.js
@@ -6,7 +6,7 @@ const projectModel = require('./projects-model')
 router.get('/', async(req, res) => {
     try{
         const projects = await projectModel.listProjects()
-        return res.status(200).json({projects})
+        return res.status(200).json(projects)
     }
     catch(err){
         return res.status(500).json({ message: err.message })
